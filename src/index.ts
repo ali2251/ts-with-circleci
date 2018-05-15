3ad8af101dac62589e25b38b00a59753d9d17dd0
Ali Azam <ali.azam@kcl.ac.uk>
2018-05-15 23:42:30 +0100

    added console.log

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import app from './app'
 
 const HOST = process.env.HOST || 'localhost'
 const PORT = process.env.PORT || '3000'
+console.log('port is: ' + PORT)
 
 app.listen(parseInt(PORT, 10), HOST, () => {
   debug('server')(`running at http://${HOST}:${PORT}`)
